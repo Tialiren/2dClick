ae2554ec59610f3310b1fc156f755f9378c5675a
tiali <tialiren@gmail.com>
2019-06-09 19:51:36 +0300

    button, sprite click

--- Assets/Scripts/MainController.cs
+++ Assets/Scripts/MainController.cs
@@ -7,12 +7,33 @@ public class MainController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _textField.text = _count.ToString();
     }
+
+    public static int _count;
+    
+    public static int Count { get => _count; set => _count = value; }
+
+
+    public UnityEngine.UI.Text _textField;
+    public UnityEngine.UI.Text TextField { get => _textField; set => _textField = value; }
+
+    
+
+    public static void CalcClick()
+    {
+        Count++;
+    }
+
+    public void CalcClick2()
+    {
+        Count++;
+    }
+
 }
